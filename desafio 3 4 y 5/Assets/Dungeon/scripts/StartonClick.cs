41b09786642670582fcf0837b3377d544e056dee
gabriela <gafacrae@gmail.com>
2024-05-19 15:29:39 -0300

    assets
    .

---
+++ desafio 3 4 y 5/Assets/Dungeon/scripts/StartonClick.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartonClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Start()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
---
+++ desafio 3 4 y 5/Assets/canvasStart.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class canvasStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void jugar()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    
+}
